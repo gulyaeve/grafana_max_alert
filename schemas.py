287a0298bfb9e467ca31977ff065cebfe80e441a
Eugene Gulyaev <gulyaeve@vk.com>
2026-06-02 10:42:42 +0300

    New scheme

--- schemas.py
+++ schemas.py
@@ -2,11 +2,18 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class GrafanaAnnotations(BaseModel):
-    summary: Optional[str] = None
-    description: Optional[str] = None
 
 
 class GrafanaPayload(BaseModel):
-    annotations: Optional[GrafanaAnnotations]
-    labels: Optional[dict] = None
+    receiver: str
+    status: str
+
+    alerts: list
+    groupLabels: dict
+
+    commonLabels: dict
+
+    commonAnnotations: dict
+    externalURL: str
+    groupKey: str
+    truncatedAlerts: int
